db5d7d2bcd0750623da6444580d6770f51ce4035
Zjoe91 <joseph.awad.91@gmail.com>
2023-11-21 15:46:24 +0100

    conflict fixed

--- potato.py
+++ potato.py
@@ -14,12 +14,6 @@ def say_hello ():
 def say_goodbye():
     print("goodbye")
 
-<<<<<<< HEAD
 def ask_moms_name():
     name= input("tell me your mom's name")
     print("hello"+name)
-=======
-def ask_name():
-    name = input()
-    print("name")
->>>>>>> 5939af05ea48f0460dba33d8cd2842c24e3a2c20
